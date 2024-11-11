ceac5227ed8a2be4cfcc8c5e90b1e237bf5f07c1
Pasindu shanuka <143288490+PShanuKa@users.noreply.github.com>
2024-11-11 17:48:50 +0530

    update

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -11,17 +11,18 @@ export const authMiddleware = asyncHandler(async (req, res, next) => {
         const decoded =  jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findById(decoded?.id);
         if(!user){
-          throw new Error("Not Authorized, Please Login Again");
+          res.status(401).json({ success: false, message: "Not Authorized, Please Login Again" });
         }
         req.user = user;
         next();
       }
     } catch (error) {
       console.log(error)
-      throw new Error("Not Authorized, Please Login Again");
+     res.status(401).json({ success: false, message: "Not Authorized, Please Login Again" });
     }
   } else {
-    throw new Error("There is no token attached to the header...");
+    res.status(401).json({ success: false, message: "Not Authorized, Please Login Again" });
+    // throw new Error("There is no token attached to the header...");
   }
 });
 
